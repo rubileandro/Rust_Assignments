98aa938c26635fe04177f20e32d5347104224c30
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2024-02-16 17:06:17 +0400

    Adding main.rs
    Week 1 assignment

---
+++ main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    pi(); // Call the pi function to print the value of pi
+}
+
+// Print out the value of pi with 10 decimal places
+fn pi() {
+    let pi = 3.14159265359;
+    println!("Pi is roughly {:.10}", pi);
+}
